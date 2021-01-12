5bb2335edbee860af95934451da78bd09a52a4c9
Sadman Hasan <sadman.h@live.com>
2021-01-11 16:22:50 -0800

    + const baseUrl,appId, tempConverter()

--- app/src/main/java/com/sadmanhasan/forecast/Generic.kt
+++ app/src/main/java/com/sadmanhasan/forecast/Generic.kt
@@ -2,10 +2,15 @@ package com.sadmanhasan.forecast
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.roundToInt
 
 class Generic {
 
     companion object {
+
+        const val baseUrl: String = "https://api.openweathermap.org/data/2.5/"
+        const val appId: String = "9cd7f16ef1ce56b903a9fdd168e7952f"
+
         fun formatTime(dt: String): String {
             val calendar = Calendar.getInstance()
             val tz = TimeZone.getDefault()
@@ -13,6 +18,10 @@ class Generic {
             val sdf = SimpleDateFormat("hh:mm a", Locale.getDefault())
             val currTimeZone = Date(dt.toLong() * 1000)
             return sdf.format(currTimeZone)
+        }
+
+        fun tempConvert(temp: Double): String {
+            return (temp - 273.15).roundToInt().toString() + " °C"
         }
     }
 }
--- app/src/main/java/com/sadmanhasan/forecast/ui/today/TodayFragment.kt
+++ app/src/main/java/com/sadmanhasan/forecast/ui/today/TodayFragment.kt
@@ -45,12 +45,12 @@ class TodayFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getCurrentWeather(city: String) {
-        Fuel.get("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=9cd7f16ef1ce56b903a9fdd168e7952f")
+        Fuel.get("${Generic.baseUrl}weather?q=$city&APPID=${Generic.appId}")
             .responseObject(TodayModel.Deserializer()) { _, _, result ->
                 val (currentWeatherModel, _) = result
                 if (currentWeatherModel != null) {
                     val icon = currentWeatherModel.weather[0].icon
-                    println(icon)
+
                     Glide.with(this)
                         .load("https://openweathermap.org/img/wn/$icon@4x.png")
                         .into(img_today_weather)
@@ -60,16 +60,15 @@ class TodayFragment : Fragment() {
                     val formatted = current.format(formatter)
                     today_date.text = formatted
 
-
-                    today_temp.text = ((currentWeatherModel.main.temp - 273.15).roundToInt().toString() + " °C")
+                    today_temp.text = Generic.tempConvert(currentWeatherModel.main.temp)
                     today_weather.text = currentWeatherModel.weather[0].description
 
                     today_feels_like.text =
-                        ("Feels like " + (currentWeatherModel.main.feels_like - 273.15).roundToInt().toString() + " °C")
+                        ("Feels like " + Generic.tempConvert(currentWeatherModel.main.feels_like))
                     today_maximum.text =
-                        ("Max " + (currentWeatherModel.main.temp_max - 273.15).roundToInt().toString() + " °C")
+                        ("Max " + Generic.tempConvert(currentWeatherModel.main.temp_max))
                     today_minimum.text =
-                        ("Min " + (currentWeatherModel.main.temp_min - 273.15).roundToInt().toString() + " °C")
+                        ("Min " + Generic.tempConvert(currentWeatherModel.main.temp_min))
 
                     today_sunrise.text = Generic.formatTime(currentWeatherModel.sys.sunrise)
                     today_sunset.text = Generic.formatTime(currentWeatherModel.sys.sunset)
