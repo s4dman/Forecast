2c749652d8fdfcd1c3cd8946be23cbb1cbe41ff7
Sadman Hasan <sadman.h@live.com>
2021-01-17 00:52:17 -0800

    + titlebar updated with location name

--- app/src/main/java/com/sadmanhasan/forecast/ui/today/TodayFragment.kt
+++ app/src/main/java/com/sadmanhasan/forecast/ui/today/TodayFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.github.kittinunf.fuel.Fuel
 import com.sadmanhasan.forecast.Generic
+import com.sadmanhasan.forecast.MainActivity
 import com.sadmanhasan.forecast.R
 import com.sadmanhasan.forecast.model.TodayModel
 import kotlinx.android.synthetic.main.fragment_today.*
@@ -20,24 +21,29 @@ import kotlin.math.roundToInt
 
 class TodayFragment : Fragment() {
 
+    companion object {
+        var cityName: String = ""
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_today_new, container, false)
+        return inflater.inflate(R.layout.fragment_today, container, false)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        btn_search.setOnClickListener {
-//            val city: String = edit_city_name.text.toString()
-//            getCurrentWeather(city)
-//
-//        }
+        cityName = Generic.getSharedPref(requireContext(), "city_name")
+
+        if (activity != null) {
+            (activity as MainActivity).supportActionBar?.title = cityName
+        }
+
+        getCurrentWeather(cityName)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -55,36 +61,36 @@ class TodayFragment : Fragment() {
 
                     Glide.with(this)
                         .load("https://openweathermap.org/img/wn/$icon@4x.png")
-                        .into(img_today_weather1)
+                        .into(img_today_weather)
 
                     val current = LocalDateTime.now()
                     val formatter = DateTimeFormatter.ofPattern("EEEE hh:mm a, MMMM d, y")
                     val formatted = current.format(formatter)
                     text_today_date.text = formatted
 
-                    today_temp.text = Generic.tempConvert(todayModel.main.temp)
-                    today_weather.text = todayModel.weather[0].description
+                    text_today_temp.text = Generic.tempConvert(todayModel.main.temp)
+                    text_today_weather.text = todayModel.weather[0].description.capitalize()
 
-                    today_feels_like.text =
+                    text_today_feels_like.text =
                         ("Feels like " + Generic.tempConvert(todayModel.main.feels_like))
-                    today_maximum.text =
-                        ("Max " + Generic.tempConvert(todayModel.main.temp_max))
-                    today_minimum.text =
-                        ("Min " + Generic.tempConvert(todayModel.main.temp_min))
+                    text_today_maxmin.text =
+                        (Generic.tempConvert(todayModel.main.temp_max) + " / " + Generic.tempConvert(
+                            todayModel.main.temp_min
+                        ))
 
-                    today_sunrise.text = Generic.formatTime(todayModel.sys.sunrise)
-                    today_sunset.text = Generic.formatTime(todayModel.sys.sunset)
+                    text_today_sunrise.text = Generic.formatTime(todayModel.sys.sunrise)
+                    text_today_sunset.text = Generic.formatTime(todayModel.sys.sunset)
 
                     val windDir =
                         arrayOf("↑N", "↗NE", "→E", "↘SE", "↓S", "↙SW", "←W", "↖NW")
                     val wind = ((todayModel.wind.deg / 45).toDouble().roundToInt() % 8)
-                    today_wind.text = ((todayModel.wind.speed * 3.6).roundToInt()
+                    text_today_wind.text = ((todayModel.wind.speed * 3.6).roundToInt()
                         .toString() + " km/h " + windDir[wind])
 
-                    today_humidity.text = (todayModel.main.humidity + "%")
-                    today_visibility.text =
+                    text_today_humidity.text = (todayModel.main.humidity + "%")
+                    text_today_visibility.text =
                         ((todayModel.visibility / 1000).toString() + " km")
-                    today_pressure.text =
+                    text_today_pressure.text =
                         ((todayModel.main.pressure / 10).toString() + " kPa")
 
                 }
